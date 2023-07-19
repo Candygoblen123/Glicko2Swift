83d11d15ab7460986a0fff4487be5a427803c043
Andrew Glaze <andrew.glaze76@gmail.com>
2023-07-19 15:58:31 +0000

    fix issues with tests

--- Tests/Glicko2SwiftTests/Glicko2SwiftTests.swift
+++ Tests/Glicko2SwiftTests/Glicko2SwiftTests.swift
@@ -16,45 +16,77 @@ final class Glicko2SwiftTests: XCTestCase {
 
     func testCCALeauge() throws {
         var teams = [String: Player]()
+        var histData = [String:[(power: Double, powerDelta: Double, gamePowerDelta: [Double])]]()
         let csv = try! CSV<Enumerated>(url: URL(fileURLWithPath: "CCAResults.csv"))
         let weeks = csv.columns![1..<csv.columns!.endIndex]
             .split(whereSeparator: { $0.rows.allSatisfy({ Int($0) != nil })})
             .filter({ !$0.isEmpty })
             .map({ $0.map({ $0.rows })})
-        for week in weeks {
+        for (j, week) in weeks.enumerated() {
+            print("Week \(j + 1)")
             for i in 0..<week[0].count {
-                var matches = [(Player, GameResult)]()
-                var invMatches = [(Player, GameResult)]()
-                let teamAlpha = teams[week[1][i], default: Player()]
-                let teamBeta = teams[week[2][i], default: Player()]
+                var teamAlpha = teams[week[1][i], default: Player()]
+                var teamBeta = teams[week[2][i], default: Player()]
 
-                let games = week[0][i].split(separator: "-").map({Int($0)!})
-                var flag = true
-                for game in games {
-                    for _ in 0..<game {
-                        if flag {
-                            matches.append((teamBeta, GameResult.win))
-                            invMatches.append((teamAlpha, GameResult.loss))
-                        } else {
-                            matches.append((teamBeta, GameResult.loss))
-                            invMatches.append((teamAlpha, GameResult.win))
-                        }
+                let games = Array(week[0][i]).map({ result in
+                    switch result {
+                        case "W": return GameResult.win
+                        case "L": return GameResult.loss
+                        default: return GameResult.win
                     }
-                    flag.toggle()
-                }
-
+                })
                 let glicko = Glicko2(0.5)
-
-                teams[week[1][i]] = glicko.calculateRating(teamAlpha, matches: matches)
-                teams[week[2][i]] = glicko.calculateRating(teamBeta, matches: invMatches)
+                var alphaScoreDiffs = [Double]()
+                var betaScoreDiffs = [Double]()
+                for game in games {
+                    let tmpAlpha = teamAlpha
+                    let tmpBeta = teamBeta
+                    teamAlpha = glicko.calculateRating(teamAlpha, matches: [(teamBeta, game)])
+                    teamBeta = glicko.calculateRating(teamBeta, matches: [(tmpAlpha, (game == .win ? .loss : .win))])
+                    alphaScoreDiffs.append(round((teamAlpha.rating - tmpAlpha.rating) * 1e2) / 1e2)
+                    betaScoreDiffs.append(round((teamBeta.rating - tmpBeta.rating) * 1e2) / 1e2)
+                }
+                histData[week[1][i], default: [(Double, Double, [Double])]()].append((teams[week[1][i], default: Player()].rating, round((teamAlpha.rating - teams[week[1][i], default: Player()].rating) * 1e2) / 1e2, alphaScoreDiffs))
+                histData[week[2][i], default: [(Double, Double, [Double])]()].append((teams[week[2][i], default: Player()].rating, round((teamBeta.rating - teams[week[1][i], default: Player()].rating) * 1e2) / 1e2, betaScoreDiffs))
+                teams[week[1][i]] = teamAlpha
+                teams[week[2][i]] = teamBeta
             }
-            //print(teams["Splatlanders"]!.rating)
         }
-        let powers = teams.reduce(into: [String:Double](), { $0[$1.key] = $1.value.rating })
+        var newCSV = "Rank,Team,Final power,"
+        for i in (1...weeks.count).reversed() {
+            newCSV.append("Power at start of Week \(i),Change in power,")
+            for j in (1...7) {
+                newCSV.append("Change in game \(j),")
+            }
+        }
+        newCSV += "\n"
+
+
         var count = 1
-        for (name, power) in powers.sorted(by: { $0.value > $1.value }) {
-            print("\(count): \(name) : \(power)")
+        let data = histData.reduce(into: [(name: String, power: Double, histData: [(power: Double, powerDelta: Double, gamePowerDelta: [Double])])](), { $0.append(($1.key, teams[$1.key]!.rating, $1.value))})
+        for (name, power, history) in data.sorted(by: { $0.power > $1.power }) {
+            newCSV.append("\(count),\"\(name)\",\"\(power)\",")
+            for (power, powerDelta, gamePowerDelta) in history.reversed() {
+                newCSV.append("\"\(power)\",\"")
+                if powerDelta > 0 {
+                    newCSV.append("+")
+                }
+                newCSV.append("\(powerDelta)\",")
+                for delta in gamePowerDelta {
+                    newCSV.append("\"")
+                    if delta > 0 {
+                        newCSV.append("+")
+                    }
+                    newCSV.append("\(delta)\",")
+                }
+                for _ in gamePowerDelta.count..<7 {
+                    newCSV.append(",")
+                }
+            }
+            newCSV.append("\n")
             count += 1
         }
+
+        try! newCSV.write(toFile: "newCSV.csv", atomically: true, encoding: .utf8)
     }
 }
